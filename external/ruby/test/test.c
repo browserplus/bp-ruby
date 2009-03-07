54d685d93e1c59e60003317d1d55946d68d816f6
Lloyd Hilaiel <lloyd@hilaiel.com>
2009-03-07 14:49:56 -0700

    add one more level of hier, make room for tests.

--- external/ruby/test/test.c
+++ external/ruby/test/test.c
@@ -3,11 +3,8 @@
 
 RUBY_GLOBAL_SETUP
 
-
-//
 static void runInterpreter(int argc, char ** argv)
 {
-
     ruby_sysinit(&argc, &argv);
     {
         RUBY_INIT_STACK;
@@ -15,8 +12,13 @@ static void runInterpreter(int argc, char ** argv)
 
         int error = 0; 
         // now do something 
-        printf("evaluating shtuff\n"); 
+        ruby_incpush("../ruby_build_output/lib/ruby/1.9.1");
+// not found and crash
+        rb_eval_string_protect("require 'digest/sha2'", &error);
         rb_eval_string_protect("foo = \"world\"\nputs \"hi #{foo}\"", &error); 
+        printf("evaluated shtuff: %d\n", error); 
+        rb_eval_string_protect("foo = \"world\"\nputs \"hello #{foo}\"",
+                               &error); 
         printf("evaluated shtuff: %d\n", error); 
     } 
     (void) ruby_finalize();
