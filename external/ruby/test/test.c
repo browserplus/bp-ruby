51194f36cde742ad3b0391b6890a780e69ed224e
Lloyd Hilaiel <lloyd@hilaiel.com>
2009-03-07 14:16:54 -0700

    now the embedding sample actually runs.

--- external/ruby/test/test.c
+++ external/ruby/test/test.c
@@ -1,34 +1,24 @@
 #include "ruby-1.9.1/ruby.h"
 #include <assert.h>
 
-// load compiled in extensions.
-//extern "C" {
-    extern void Init_ext _((void));
-//};
-void RUBY_load_builtins(void)
-{
-    Init_ext();
-}
+RUBY_GLOBAL_SETUP
+
 
 //
 static void runInterpreter(int argc, char ** argv)
 {
-    ruby_init();
-    ruby_options(argc, argv);
-    ruby_script(argv[0]);
 
+    ruby_sysinit(&argc, &argv);
+    {
+        RUBY_INIT_STACK;
+        ruby_init();
 
-    RUBY_load_builtins(); 
-
-    ruby_incpush("../ruby_build_output/lib/ruby/1.9.1");
-    rb_require("digest/md5");
-
-    int error = 0;
-    // now do something
-    printf("evaluating shtuff\n");
-    rb_eval_string("foo = 'world'");
-    rb_eval_string_protect("puts \"hello #{foo}\"", &error);
-    printf("evaluated shtuff: %d\n", error);
+        int error = 0; 
+        // now do something 
+        printf("evaluating shtuff\n"); 
+        rb_eval_string_protect("foo = \"world\"\nputs \"hi #{foo}\"", &error); 
+        printf("evaluated shtuff: %d\n", error); 
+    } 
     (void) ruby_finalize();
 }
 
