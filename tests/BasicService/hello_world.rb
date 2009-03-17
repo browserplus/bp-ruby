cb08d172944ee589f1f195d785095984f58c23a8
Lloyd Hilaiel <lloyd@hilaiel.com>
2009-03-17 17:50:44 -0600

    pair back the magic a bit, still exposing the "transaction" arg, and the "arguments" arg - we'll iterate here for a bit (on the service definition semantics), but at least there's a clear path forward to get things up and running

--- tests/BasicService/hello_world.rb
+++ tests/BasicService/hello_world.rb
@@ -4,18 +4,18 @@
 # http://github.com/why/fakeplus/blob/master/stdlib/bp_hacks.rb
 #
 # design credit? why, the lucky stiff, of course.
+#
 
-class Test
+class HelloWorld
   bp_version "1.0.0"
-  bp_doc "Hello world service/corelet thingy."
+  bp_doc "A hello world test service for BrowserPlus."
 
-  def hello(code)
-    "hello world"
+  def hello(trans, args)
+    args[:cb].invoke("Hi there #{args[:who]}") if args.has_key? :cb
+    trans.complete("hello #{args[:who]}")
   end
 
-  bp_doc :hello, 
-    "return the string, 'hello world'.  original, eh?"
+  bp_doc :hello, "return the string, 'hello world'.  original, eh?
+                  <who: string> who to say hello to.
+                  [cb: callback] a callback to invoke"
 end
-
-# and this should be optional?!
-RubyServiceDefinition = Test.to_service_def
