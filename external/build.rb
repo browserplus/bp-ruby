90541eb7f39e7cfb9e66eb1cb3b74eacada4c13c
Lloyd Hilaiel <lloyd@hilaiel.com>
2009-12-21 18:13:38 -0800

    help the poor fool building this along, check for the presence of bison.exe and sed.exe

--- external/build.rb
+++ external/build.rb
@@ -1,5 +1,31 @@
 #!/usr/bin/env ruby
 
+require 'fileutils'
+require 'rbconfig'
+include Config
+
+if CONFIG['arch'] =~ /mswin/
+  rv = system("sed --help > devenv.out 2>&1")    
+  FileUtils.rm_f("devenv.out")
+  if !rv
+    puts "************************************************************"
+    puts "sed.exe not in search path, install from (and then RENAME!):"
+    puts "http://gnuwin32.sourceforge.net/packages/sed.htm"
+    puts "************************************************************"
+    exit -1
+  end
+
+  rv = system("bison -h > devenv.out 2>&1")    
+  FileUtils.rm_f("devenv.out")
+  if !rv
+    puts "************************************************************"
+    puts "bison.exe not in search path, install from (and then RENAME!):"
+    puts "http://gnuwin32.sourceforge.net/packages/bison.htm"
+    puts "************************************************************"
+    exit -1
+  end
+end
+
 require "bakery/ports/bakery"
 
 $order = {
