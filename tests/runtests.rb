eebca7f8026a8829d696e4734be770c580c814b1
Lloyd Hilaiel <lloyd@hilaiel.com>
2010-06-04 19:48:11 -0600

    w00t.  a first passing test

--- tests/runtests.rb
+++ tests/runtests.rb
@@ -12,9 +12,12 @@ class TestFileAccess < Test::Unit::TestCase
   def teardown
   end
 
-  def test_basic_service
-    BrowserPlus.run("BasicService", "../build/RubyInterpreter") { |s|
-
+  def test_file_checksum
+    BrowserPlus.run("FileChecksum", "../build/RubyInterpreter") { |s|
+      curDir = File.dirname(__FILE__)
+      textfile_path = File.expand_path(File.join(curDir, "services.txt"))
+      textfile_uri = (( textfile_path[0] == "/") ? "file://" : "file:///" ) + textfile_path
+      assert_equal "babc871bf6893c8313686e31cb87816a",  s.md5({:file => textfile_uri})
     }
   end
 end
